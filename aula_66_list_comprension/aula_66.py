c181a54fa9c6a8ae760129b786b11571c891fe0a
JonasFiechter <jonasrfiechter@gmail.com>
2022-07-02 17:12:41 -0300

    .

--- aula_66_list_comprension/aula_66.py
+++ aula_66_list_comprension/aula_66.py
@@ -4,11 +4,12 @@ ex1 = [variavel for variavel in l1]
 ex2 = [v * 2 for v in l1]
 ex3 = [(v, v2) for v in l1 for v2 in range(3)]
 
-l2 = ['Luiz', 'Mauro', 'Maria']
-ex4 = [v.replace('a', '@').upper() for v in l2]
-print(ex4)
+print(ex3)
+# l2 = ['Luiz', 'Mauro', 'Maria']
+# ex4 = [v.replace('a', '@').upper() for v in l2]
+# print(ex4)
 
-l3 = '1092jdiajodma01923012kmkmadlkmasd'
-ex5 = [l3[i: i + 5] for i in range(0, len(l3), 5)]
+# l3 = '1092jdiajodma01923012kmkmadlkmasd'
+# ex5 = [l3[i: i + 5] for i in range(0, len(l3), 5)]
 
-print(ex5)
+# print(ex5)
